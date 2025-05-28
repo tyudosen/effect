e9bbfe5a57d63e5ba0c8d729389e042d14d28684
toyo <toyo@Toyos-MBP.lan>
2025-05-28 10:39:08 -0500

    catch and handle error

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-import { Console, Effect } from "effect";
+import { Console, Effect, pipe } from "effect";
 
 const fetchRequest = Effect.tryPromise(
-	() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
+	() => fetch("https://pokeapi.co/api/v2/pokemon/garchom/")
 )
 
 const jsonResponse = (response: Response) => Effect.tryPromise(
 	() => response.json()
 )
 
+const savePokemon = (pokemon: string) => Effect.tryPromise(
+	() => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+)
 
-const main = Effect.flatMap(
+
+const main = pipe(
 	fetchRequest,
-	jsonResponse
-).pipe(Effect.tap((res) => Console.log(res))
+	Effect.flatMap(jsonResponse),
+	Effect.tap((res) => Console.log(res)),
+	Effect.catchTag(
+		"UnknownException",
+		() => savePokemon("pikachu").pipe(Effect.flatMap(jsonResponse))
+	),
+	Effect.tap((res) => Console.log(res)),
+
+
 )
 
 Effect.runPromise(main)
