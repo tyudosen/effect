0747e41d5aad245d13a915a3327e87cf9416121e
toyo <toyo@MacBookPro.lan>
2025-05-28 00:46:54 -0500

    collect errors using tryPromise

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Console, Effect } from "effect";
 
-const fetchRequest = Effect.promise(
+const fetchRequest = Effect.tryPromise(
 	() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
 )
 
-const jsonResponse = (response: Response) => Effect.promise(
+const jsonResponse = (response: Response) => Effect.tryPromise(
 	() => response.json()
 )
 
