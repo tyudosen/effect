48681f6fb3a6fc70bacae3c03d1daeaa5f104381
toyo <toyo@Toyos-MBP.lan>
2025-05-30 02:12:27 -0500

    set up live and test runtime

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,6 +1,6 @@
 import { afterAll, afterEach, beforeAll, expect, it } from 'vitest'
 import { PokeApi } from '../PokeApi';
-import { Effect, Layer } from 'effect';
+import { Effect, Layer, ManagedRuntime } from 'effect';
 import { server } from './node';
 import { ConfigProviderLayer } from './configProvider';
 
@@ -13,6 +13,7 @@ const MainLayer = PokeApi.Default.pipe(
 	// 👇 Provide the `ConfigProvider` layer to `PokeApi.Live`
 	Layer.provide(ConfigProviderLayer),
 );
+const TestRuntime = ManagedRuntime.make(MainLayer)
 
 
 
@@ -21,11 +22,9 @@ const program = Effect.gen(function* () {
 	return yield* pokeApi.getPokemon;
 });
 
-// 👇 Provide the `PokeApi` live implementation to test
-const main = program.pipe(Effect.provide(MainLayer));
 
 it("returns a valid pokemon", async () => {
-	const response = await Effect.runPromise(main);
+	const response = await TestRuntime.runPromise(program)
 	expect(response).toEqual({
 		id: 1,
 		height: 10,
